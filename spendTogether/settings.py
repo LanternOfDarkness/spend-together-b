ff75f44c33d20ad4fa8cb9c43cc7f4ce034e5b36
hellookittyyy <katya.shapovalova3012@gmail.com>
2025-02-09 21:45:42 +0200

    Setup database. Added .env

--- spendTogether/settings.py
+++ spendTogether/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4o_c@2$v^^m$(=ha9lf)00+--&8q)er1@$hs5q0hu=kyg3lm7*'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
