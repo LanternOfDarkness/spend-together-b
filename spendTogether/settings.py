433d9c2f878f672c0f0e8dfcb007231664831108
hellookittyyy <katya.shapovalova3012@gmail.com>
2025-02-09 22:42:19 +0200

    Made setup for auth.

--- spendTogether/settings.py
+++ spendTogether/settings.py
@@ -40,7 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
